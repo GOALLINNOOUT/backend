9c1318246b039f2c3cfe0467a1033a40e8788da9
ADELA009 <PHILIPADELA12@outlook.com>
2025-07-26 18:57:42 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -97,7 +97,7 @@ router.post('/login', async (req, res) => {
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ error: 'Incorrect password. Please try again.' });
+      return res.status(400).json({ error: 'Incorrect password.' });
     }
     // Check if the user is suspended or blacklisted
     if (user.status === 'suspended' || user.status === 'blacklisted') {
